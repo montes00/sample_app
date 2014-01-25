46cde2efff54fc4148b7f53a547f475a2ea7571d
futaana@moonwater <futaana@moonwater>
2014-01-25 22:50:05 +0100

    [5_filling-in-layout] Better test codes.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,24 +2,17 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'Sample App'" do
-      visit root_path
-      page.should have_selector('h1', :text => 'Sample App')
-    end
+    it { should have_selector('h1', text: 'Sample App')}
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: '| Home') }
 
-    it "should have the base title" do
-      visit root_path
-      page.should have_selector('title', :text => "#{base_title}")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', :text => "| Home")
-    end
   end
 
   describe "Help page" do
